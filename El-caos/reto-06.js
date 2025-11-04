f1eaaa5efec6d21ad242fa1098439ffcdf3e5cb9
Tomas B. Ezequiel Guzman <tomasgz.dev@gmail.com>
2025-11-04 09:54:19 -0300

    Reto 06: El Caos – Ruleta mortal de Art the Clown | Solución óptima en JS

---
+++ El-caos/reto-06.js
@@ -0,0 +1,15 @@
+function surviveRoulette(n, k) {
+  let survivor = 0;
+
+  for (let i = 1; i <= n; i++) {
+    survivor = (survivor + k) % i;
+  }
+  return survivor;
+}
+
+// 🧪 Pruebas rápidas
+console.log(surviveRoulette(4, 2));  // 0
+console.log(surviveRoulette(5, 3));  // 3
+console.log(surviveRoulette(5, 10)); // 3
+console.log(surviveRoulette(7, 5));  // 6
+console.log(surviveRoulette(10, 2)); // 4
